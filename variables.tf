3aae6e8b0d5045864209198c168949be10e958ab
Harshada Deo <140598128+harshadadeo2810@users.noreply.github.com>
2025-06-28 14:44:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,53 @@
-variable "region" {
-  description = "AWS region"
+variable "vpc_name" {
+  description = "VPC name"
   type        = string
-  default     = "us-west-1"
+  default     = "eks-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["us-west-1a", "us-west-1c"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+  default     = "eks-cluster"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Max number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "min_size" {
+  description = "Min number of nodes"
+  type        = number
+  default     = 1
 }
