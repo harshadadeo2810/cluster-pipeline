394e3449ef6926dab6c8e20871e8c5b27f506d05
Harshada Deo <140598128+harshadadeo2810@users.noreply.github.com>
2025-06-28 14:45:17 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
 output "cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS Cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
-
-output "node_group_role_arn" {
-  value = module.eks.eks_managed_node_groups["default"].iam_role_arn
+  description = "EKS Cluster endpoint"
+  value       = module.eks.cluster_endpoint
 }
