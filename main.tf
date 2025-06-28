aec6769e550825404355f4dc2450a172bcbbfc46
Harshada Deo <140598128+harshadadeo2810@users.noreply.github.com>
2025-06-28 14:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,59 +4,48 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.azs[0] != "" ? substr(var.azs[0], 0, 9) : "us-west-1"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name                 = var.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.azs
+  public_subnets       = var.public_subnets
+  private_subnets      = var.private_subnets
 
-  azs             = ["us-west-1a", "us-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.37.1"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.3"
 
-  cluster_name    = "eks-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  enable_irsa = true
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
     default = {
+      desired_capacity = var.desired_capacity
+      max_capacity     = var.max_size
+      min_capacity     = var.min_size
+
       instance_types = ["t3.medium"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-
-      capacity_type = "ON_DEMAND"
+      ami_type       = "AL2_x86_64"
     }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
